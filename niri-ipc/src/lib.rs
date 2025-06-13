88beee1eca4d4e5049e4f00864e93b247fb134e9
Yury Shvedov <yury.shvedov@kaspersky.com>
2025-06-13 22:24:02 +0300

    window: allow to set per-window view offset
    This allows to set view_offset for window, which makes workspace to
    shift it view while window is focused.
    
    Change-Id: I8a2fad7f16fbf85367883f12340373fc89cd0e09

--- niri-ipc/src/lib.rs
+++ niri-ipc/src/lib.rs
@@ -943,6 +943,17 @@ pub enum Action {
         #[cfg_attr(feature = "clap", arg(long))]
         path: Option<String>,
     },
+    /// Set view offset of window.
+    ViewOffset {
+        /// Id of the window to set view offset.
+        ///
+        /// If `None`, uses the focused window.
+        #[cfg_attr(feature = "clap", arg(long))]
+        id: Option<u64>,
+        /// View offset to set
+        #[cfg_attr(feature = "clap", arg(long))]
+        offset: f64,
+    },
 }
 
 /// Change in window or column size.
