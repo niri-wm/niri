f0390cd3eac98c5a18e1c493cadbd68e02f23c53
Benjamin Bäumler <benjamin.baeumler@qqwa.de>
2026-01-23 18:28:03 +0100

    Add tablet option map-to-focused-output

--- niri-config/src/lib.rs
+++ niri-config/src/lib.rs
@@ -701,6 +701,7 @@ mod tests {
 
                 tablet {
                     map-to-output "eDP-1"
+                    map-to-focused-output
                     calibration-matrix 1.0 2.0 3.0 \
                                        4.0 5.0 6.0
                 }
@@ -1093,6 +1094,7 @@ mod tests {
                     map_to_output: Some(
                         "eDP-1",
                     ),
+                    map_to_focused_output: true,
                     left_handed: false,
                 },
                 touch: Touch {
