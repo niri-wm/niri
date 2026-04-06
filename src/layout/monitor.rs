629f6f4cf0f834ebcb9c89b77a9c82b63da820be
Chaitanya Dubakula <chaitanyarp4@gmail.com>
2026-04-06 23:06:51 +0530

    zoom (will split again later)

--- src/layout/monitor.rs
+++ src/layout/monitor.rs
@@ -20,6 +20,7 @@ use super::workspace::{
 use super::{compute_overview_zoom, ActivateWindow, HitType, LayoutElement, Options};
 use crate::animation::{Animation, Clock};
 use crate::input::swipe_tracker::SwipeTracker;
+use crate::layout::ZoomTransition;
 use crate::niri_render_elements;
 use crate::render_helpers::renderer::NiriRenderer;
 use crate::render_helpers::shadow::ShadowRenderElement;
@@ -31,6 +32,7 @@ use crate::utils::transaction::Transaction;
 use crate::utils::{
     output_size, round_logical_in_physical, round_logical_in_physical_max1, ResizeEdge,
 };
+use crate::zoom::OutputZoomState;
 
 /// Amount of touchpad movement to scroll the height of one workspace.
 const WORKSPACE_GESTURE_MOVEMENT: f64 = 300.;
@@ -88,6 +90,10 @@ pub struct Monitor<W: LayoutElement> {
     pub(super) options: Rc<Options>,
     /// Layout config overrides for this monitor.
     layout_config: Option<niri_config::LayoutPart>,
+    /// In-progress zoom transition state for this monitor.
+    pub(super) zoom_transition: ZoomTransition,
+    /// Per-output zoom snapshot (level, focal, locked, transitioning).
+    pub(super) zoom_state: OutputZoomState,
 }
 
 #[derive(Debug)]
@@ -327,6 +333,8 @@ impl<W: LayoutElement> Monitor<W> {
         let ws = Workspace::new(output.clone(), clock.clone(), options.clone());
         workspaces.push(ws);
 
+        let zoom_state = OutputZoomState::new_for_output(&output);
+
         Self {
             output_name: output.name(),
             output,
@@ -346,6 +354,8 @@ impl<W: LayoutElement> Monitor<W> {
             base_options,
             options,
             layout_config,
+            zoom_transition: ZoomTransition::default(),
+            zoom_state,
         }
     }
 
